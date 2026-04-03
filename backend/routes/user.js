7ba1d2ac62118f8bcb9a2987ddbc59e992dc9c73
pranjal2876 <pranjal2876@gmail.com>
2026-04-03 09:47:37 +0530

    fixed final case issue

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/userModel");
+const User = require("../models/usermodel");
 
 // CREATE / LOGIN USER
 router.post("/create", async (req, res) => {
--- backend/wallet.js
+++ backend/wallet.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("./models/userModel");
+const User = require("./models/usermodel");
 
 // GET BALANCE
 router.get("/:userId", async (req, res) => {
