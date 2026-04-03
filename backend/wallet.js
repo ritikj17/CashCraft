92efc1cf47c6ab227a6fdabaa0da9b9313ef02cc
pranjal2876 <pranjal2876@gmail.com>
2026-04-03 09:42:51 +0530

    fixed naming conflict user model

--- backend/wallet.js
+++ backend/wallet.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("./models/User");
+const User = require("./models/userModel");
 
 // GET BALANCE
 router.get("/:userId", async (req, res) => {
